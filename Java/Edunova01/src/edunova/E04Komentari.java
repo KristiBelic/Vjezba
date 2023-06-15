4e17ea6650bc4cdd12070bbff4387860532c59e4
KristiBelic <kristinovoselic@gmail.com>
2023-06-15 13:46:03 +0200

    Hello world java

---
+++ Java/Edunova01/src/edunova/E04Komentari.java
@@ -0,0 +1,11 @@
+package edunova;
+
+public class E04Komentari {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Komentari ŽĆĐĐĆŠŽĆĐ");
+
+	}
+
+}
---
+++ Java/Edunova01/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Josip
+ *
+ */
+module Edunova {
+}
